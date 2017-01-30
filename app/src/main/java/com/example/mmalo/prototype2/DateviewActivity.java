644cc850e2421251b1e12b6c6a6f36e516184d6b
Matt Malone <mmalone136@gmail.com>
2017-01-30 22:06:24 +0000

    Extended functionality to read diary data. Can now view entries by specific date.

---
+++ app/src/main/java/com/example/mmalo/prototype2/DateviewActivity.java
@@ -0,0 +1,135 @@
+package com.example.mmalo.prototype2;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.mmalo.prototype2.DB.DBHelper;
+import com.example.mmalo.prototype2.Models.DiaryData;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+/**
+ * Created by mmalo on 30/01/2017.
+ */
+
+public class DateviewActivity extends AppCompatActivity {
+
+    public ArrayList<DiaryData> entries = new ArrayList<DiaryData>();
+    public static String date;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dateview);
+
+        initVals();
+    }
+
+
+    public void initVals(){
+
+        ListView entryList = (ListView) findViewById(R.id.listEntries);
+        //uniqueDates = readUniqDates();
+        entries = readEntriesForDate(date);
+
+
+        ArrayList<String> listData = new ArrayList<String>();
+        for (DiaryData d : entries) {
+            Timestamp dTimestamp = d.getTimestamp();
+
+            String curr = d.getMeal() + " - " + dTimestamp.toString();
+            listData.add(curr);
+        }
+
+        try {
+
+            ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+
+            entryList.setAdapter(adapter);
+
+
+            entryList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    //activity.dateselected = dates.get(position);
+
+                    DiaryData curr = entries.get(position);
+                    SummaryActivity.theentry = curr;
+                    Intent i = new Intent(getBaseContext(), SummaryActivity.class);
+                    //i.putExtra("diary_data", curr);
+                    startActivity(i);
+                }
+            });
+        }catch(Exception e){
+
+            e.printStackTrace();
+            e.printStackTrace();
+        }
+
+        System.out.println("");
+    }
+
+
+
+    public ArrayList<DiaryData> readEntriesForDate(String date) {
+        DBHelper dbh = new DBHelper(getApplicationContext());
+        SQLiteDatabase db = dbh.getReadableDatabase();
+
+        String[] projection = {
+                "entry_ID", "photo_data", "comment_data", "time_stamp", "meal"
+        };
+        String select = "time_stamp Like ?";
+        String[] selArgs = {"%" + date + "%"};
+        //ArgOrder => Table,Columns, Columns From Where, Values from where, togroup, tofilter groups, sortorder
+        Cursor cursor = db.query("diary_entries", projection, select, selArgs, null, null, null);
+
+        System.out.print("");
+        ArrayList<DiaryData> entries = new ArrayList<DiaryData>();
+        ArrayList<String> comments = new ArrayList<String>();
+        ArrayList<String> times = new ArrayList<String>();
+        int i = 0;
+
+
+        if(cursor.moveToFirst()) {
+            do {
+
+                try {
+                    DiaryData curr;
+                    byte[] pic = cursor.getBlob(cursor.getColumnIndexOrThrow("photo_data"));
+                    String comm = cursor.getString(cursor.getColumnIndexOrThrow("comment_data"));
+
+                    String tID = cursor.getString(cursor.getColumnIndexOrThrow("time_stamp"));
+                    String theMeal = cursor.getString(cursor.getColumnIndexOrThrow("meal"));
+
+                    Timestamp theTime = Timestamp.valueOf(tID);
+                    curr = new DiaryData(pic, comm, null, theTime, theMeal);
+
+                    entries.add(curr);
+                    i++;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    e.printStackTrace();
+
+                }
+
+
+            } while (cursor.moveToNext());
+
+        }
+
+
+        cursor.close();
+
+        return entries;
+    }
+}
--- app/src/main/java/com/example/mmalo/prototype2/SumOptions.java
+++ app/src/main/java/com/example/mmalo/prototype2/SumOptions.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import com.example.mmalo.prototype2.DB.DBHelper;
 import com.example.mmalo.prototype2.Models.DiaryData;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 
@@ -24,24 +25,39 @@ import java.util.ArrayList;
 public class SumOptions extends AppCompatActivity {
 
     public ArrayList<DiaryData> entries = new ArrayList<DiaryData>();
+    public ArrayList<String> uniqueDates = new ArrayList<String>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.summary_options);
 
-        ListView datelist = (ListView) findViewById(R.id.listDates);
+        initVals();
+    }
 
+
+    public void initVals(){
+
+        ListView datelist = (ListView) findViewById(R.id.listDates);
+        uniqueDates = readUniqDates();
         entries = readAllEntries();
 
         ArrayList<Timestamp> ts = new ArrayList<Timestamp>();
+        ArrayList<String> dt = new ArrayList<String>();
         for (DiaryData d : entries) {
-            ts.add(d.getTimestamp());
+            Timestamp curr = d.getTimestamp();
+            Date seven = new Date(curr.getTime());
+            ts.add(curr);
+
+            if(!dt.contains(seven.toString())){
+                dt.add(seven.toString());
+
+            }
 
         }
 
 
-        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ts);
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, uniqueDates);
 
         datelist.setAdapter(adapter);
 
@@ -51,13 +67,15 @@ public class SumOptions extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //activity.dateselected = dates.get(position);
 
-                DiaryData curr = entries.get(position);
-                SummaryActivity.theentry = curr;
-                Intent i = new Intent(getBaseContext(), SummaryActivity.class);
+                String curr = uniqueDates.get(position);
+                DateviewActivity.date = curr;
+                Intent i = new Intent(getBaseContext(), DateviewActivity.class);
                 //i.putExtra("diary_data", curr);
                 startActivity(i);
             }
         });
+
+
     }
 
     public ArrayList<DiaryData> readAllEntries() {
@@ -111,5 +129,74 @@ public class SumOptions extends AppCompatActivity {
         return entries;
     }
 
+
+
+
+
+
+    public ArrayList<String> readUniqDates() {
+        DBHelper dbh = new DBHelper(getApplicationContext());
+        SQLiteDatabase db = dbh.getReadableDatabase();
+
+        String[] projection = {
+                "time_stamp"
+        };
+
+        //ArgOrder => Table,Columns, Columns From Where, Values from where, togroup, tofilter groups, sortorder
+        Cursor cursor = db.query("diary_entries", projection, null, null, null, null, null);
+
+        System.out.print("");
+        ArrayList<DiaryData> entries = new ArrayList<DiaryData>();
+        //ArrayList<String> comments = new ArrayList<String>();
+        //ArrayList<String> times = new ArrayList<String>();
+        int i = 0;
+
+        ArrayList<Timestamp> times = new ArrayList<Timestamp>();
+        ArrayList<String> dates = new ArrayList<>();
+        if(cursor.moveToFirst()) {
+            do {
+
+                try {
+                    //DiaryData curr;
+                    //byte[] pic = cursor.getBlob(cursor.getColumnIndexOrThrow("photo_data"));
+                   // String comm = cursor.getString(cursor.getColumnIndexOrThrow("comment_data"));
+
+                    String tID = cursor.getString(cursor.getColumnIndexOrThrow("time_stamp"));
+                    //String theMeal = cursor.getString(cursor.getColumnIndexOrThrow("meal"));
+
+                    Timestamp theTime = Timestamp.valueOf(tID);
+                   // curr = new DiaryData(pic, comm, null, theTime, theMeal);
+                    times.add(theTime);
+
+
+
+
+                        Date seven = new Date(theTime.getTime());
+
+
+                        if(!dates.contains(seven.toString())){
+                            dates.add(seven.toString());
+
+                        }
+
+
+                   // entries.add(curr);
+                    i++;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    e.printStackTrace();
+
+                }
+
+
+            } while (cursor.moveToNext());
+
+        }
+
+
+        cursor.close();
+
+        return dates;
+    }
 
 }
